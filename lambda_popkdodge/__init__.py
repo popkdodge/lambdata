72e6ca4c9112fdaf84dcf6b27be2fc74f04220f9
SASANA KONGJAREON <popkdodge@gmail.com>
2020-07-06 18:40:37 -0400

    Adding first building package

---
+++ lambda_popkdodge/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+from . import example_module
+
+TEST = pd.DataFrame(np.ones(10))
+Y = example_module.increment(example_module.x)
