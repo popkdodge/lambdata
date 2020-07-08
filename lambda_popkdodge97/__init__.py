f55ad9fef3a1960fe656511340c1f049dbf79015
Ekaterina Romanovskaya <64712690+KathyRoma@users.noreply.github.com>
2020-07-07 18:05:19 -0700

    added review

--- lambda_popkdodge97/__init__.py
+++ lambda_popkdodge97/__init__.py
@@ -11,8 +11,10 @@ from . import example_module
 #module
 TEST = pd.DataFrame(np.ones(10))
 Y = example_module.increment(example_module.x)
-setup = pd.set_option.display.max_columns = 999
+# ER: Below was causing an error for me, don't see it being used anywhere
+#setup = pd.set_option.display.max_columns = 999
 
+# ER: these functions are not getting called anywhere in the code above, are they supposed to be in the module?
 #functions
 def check_null(X):
   return X.isnull().sum()
